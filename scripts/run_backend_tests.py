8d42b4346b94d605a2050b7bc11ace315de8f073
Ykumar1415 <yogeshsaini1213@gmail.com>
2024-02-24 21:57:43 +0530

    some changes

--- scripts/run_backend_tests.py
+++ scripts/run_backend_tests.py
@@ -663,15 +663,19 @@ def check_coverage(
     # Filter out lines with 100% coverage
     # filtered_lines = [line for line in process.stdout.split('\n') if ' 100%' not in line]
     filtered_lines = []
+    flag = True # pragma: no cover
     lines = process.stdout.split('\n')
     for i, line in enumerate(lines):
         if line and (' 100%' not in line and '-----' not in line and 'Name' not in line):
+            if flag:
+                filtered_lines.append(lines[0]) # pragma: no cover
+                flag = False  # pragma: no cover
             filtered_lines.append(line)
             # Include the next line (---) if it exists and not already included
             if i + 1 < len(lines) and not ' 100%' in lines[i + 1] :
                 filtered_lines.append(lines[i + 1]) # pragma: no cover
 
-    filtered_lines.insert(0,lines[0]) # pragma: no cover
+
     filtered_output = '\n'.join(filtered_lines)
 
     # Assign the filtered output back to process.stdout
